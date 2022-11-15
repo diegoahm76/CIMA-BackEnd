96ae891e3213bb641701c43891838efea4bb2500
jcduque94 <kamilo720@hotmail.com>
2022-11-15 16:03:12 -0500

    Change connection string

--- config/db.py
+++ config/db.py
@@ -1,5 +1,6 @@
 from sqlalchemy import create_engine, MetaData
 
-engine = create_engine("postgresql://postgres:0000@localhost:5432/CIMA-DB")
+engine = create_engine("postgresql://postgres:LNHmWoJJ57TtHlYDgcDb@sig-database.c1ur1zqj7pwl.us-east-1.rds.amazonaws.com:5432/cima")
+# engine = create_engine("postgresql://postgres:cima@localhost:5432/cima")
 meta = MetaData()
 connection = engine.connect()
